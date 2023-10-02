39381c017a9f3f8490a7bb168103bf8f5c7dd6a5
AnshuShree2004 <ianshushree@gmail.com>
2023-10-02 16:23:39 +0530

    Added login signup buttons to side bar

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,29 +1,40 @@
-
-import {AiFillCloseCircle} from 'react-icons/ai'
+import { AiFillCloseCircle } from "react-icons/ai";
 import { FiMenu } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 import Footer from "../components/Footer";
 
-
 function HomeLayout({ children }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    function changeWidth() {
-        const drawerSide = document.getElementsByClassName("drawer-side")
-        drawerSide[0].style.width = 'auto'
-
-    }
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+  const role = useSelector((state) => state?.auth?.role);
 
 
-    function hideDrawer() {
-        const element = document.getElementsByClassName("drawer-toggle")
-        element[0].checked = false
 
-        const drawerSide = document.getElementsByClassName("drawer-side")
-        drawerSide[0].style.width = '0'
+  function changeWidth() {
+    const drawerSide = document.getElementsByClassName("drawer-side");
+    drawerSide[0].style.width = "auto";
+  }
 
-    }
+  function hideDrawer() {
+    const element = document.getElementsByClassName("drawer-toggle");
+    element[0].checked = false;
 
+    const drawerSide = document.getElementsByClassName("drawer-side");
+    drawerSide[0].style.width = "0";
+  }
+
+
+  function onLogout(e) {
+   
+     e.preventDefault()
+
+     //todo
+     navigate("/")
+  }
 
   return (
     <div className="min-h-[90vh]">
@@ -32,45 +43,103 @@ function HomeLayout({ children }) {
 
         <div className="drawer-content">
           <label htmlFor="my-drawer">
-            <FiMenu onClick={changeWidth} size={"32px"}  className="font-bold text-black m-4"/>
+            <FiMenu
+              onClick={changeWidth}
+              size={"32px"}
+              className="font-bold text-white m-4"
+            />
           </label>
         </div>
 
         <div className="drawer-side w-0">
-    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-    <ul className="menu p-4 w-48 min-h-full bg-base-200 text-base-content relative sm:w-80">
+          <label
+            htmlFor="my-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu p-4 w-48 min-h-full bg-base-200 text-base-content relative sm:w-80 ">
+            <li className="w-fit absolute right-2 z-50">
+              <button onClick={hideDrawer}>
+                <AiFillCloseCircle size={"24"} />
+              </button>
+            </li>
 
-    <li className='w-fit absolute right-2 z-50'>
-      <button onClick={hideDrawer}>
-        <AiFillCloseCircle size={'24'}/>
-      </button>
-    </li>
-     
-    <li>
-    <Link to="/" >Home</Link>
-    </li>
-   
-    <li>
-    <Link to="/about" >About us</Link>
-    </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
 
-    <li>
-    <Link to="/contact" >Contact us</Link>
-    </li>
+            {
+              isLoggedIn && role === "ADMIN" && (
+                <li>
+                  <Link to="/admin/dashboard">Admin Dashboard</Link>
+                </li>
+              )
+            }
 
-    <li>
-    <Link to="/courses" >All Courses</Link>
-    </li>
-      
-    </ul>
-  </div>
+            <li>
+              <Link to="/about">About us</Link>
+            </li>
+
+            <li>
+              <Link to="/contact">Contact us</Link>
+            </li>
+
+            <li>
+              <Link to="/courses">All Courses</Link>
+            </li>
+            
+
+            
+
+
+             { isLoggedIn ?
+               ( <li className="absolute bottom-4 w-[90%] ">
+                <div className="w-full flex items-center justify-center">
+               
+               <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                <Link to="/login">
+                  Login
+                </Link>
+               </button>
+
+
+               <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                <Link to="/signup">
+                  Signup
+                </Link>
+               </button>
+
+                </div>
+                </li>) : ( <li className="absolute bottom-4 w-[90%] ">
+                <div className="w-full flex items-center justify-center">
+               
+               <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                <Link to="/user/profile">
+                  Profile
+                </Link>
+               </button>
+
+
+               <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                <Link onClick={onLogout}>
+                  Logout
+                </Link>
+               </button>
+
+                </div>
+                </li>)
+              }
+             
+           
+          </ul>
+        </div>
       </div>
 
 
       {children}
       <Footer />
     </div>
-  );
+  )
 }
 
 export default HomeLayout;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,25 +8,25 @@ function Home() {
     return (
 
         <HomeLayout>
-          <div className="pt-10 text-black flex items-center justify-center gap-10 mx-16 h-[90vh]">
+          <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[90vh]">
           
           <div className="w-1/2 space-y-6">
             <h1 className="text-5xl font-semibold">Find out best  <span className="text-yellow-400 font-bold">Online Courses</span></h1>
 
-            <p className="text-xl text-gray-700">
+            <p className="text-xl text-gray-200">
                 We have a large library of courses taught by highly skilled and qualified faculties at a very affordable cost.
             </p>
 
             <div className="space-x-6">
 
                 <Link to="/courses">
-                <button className="bg-yellow-400 px-5 py-3 rounded-md font text-lg cursor-pointer hover:bg-yellow-500 transition-all ease-in-out text-white font-semibold">
+                <button className="bg-yellow-500 px-5 py-3 rounded-md font text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out text-white font-semibold">
                     Explore courses
                 </button>
                 </Link>
 
                 <Link to="/contacts">
-                <button className=" border border-yellow-500 px-5 py-3 rounded-md font text-lg cursor-pointer hover:bg-yellow-500 transition-all ease-in-out hover:text-white font-semibold">
+                <button className=" border border-yellow-500 px-5 py-3 rounded-md font text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out  font-semibold">
                     Contact us
                 </button>
                 </Link>
